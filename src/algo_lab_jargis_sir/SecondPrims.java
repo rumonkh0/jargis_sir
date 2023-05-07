7a53a4cc8da7e9686ba223b263e9d59061ad37bd
RUMON <rummankh0@gmail.com>
2023-05-07 11:51:24 +0600

    result output file

--- src/algo_lab_jargis_sir/SecondPrims.java
+++ src/algo_lab_jargis_sir/SecondPrims.java
@@ -6,6 +6,10 @@ package algo_lab_jargis_sir;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
@@ -17,6 +21,7 @@ public class SecondPrims {
     static int node, edge, graph[][], sKey[], key[], parent[], secondParent[], printParent[], mstset[], root, total = Integer.MAX_VALUE, cTotal = 0;
 
     SecondPrims() {
+        dateWrite();
         try {
 //          File bfsfile = new File("/home/student_user/Desktop/prims.txt");
             File bfsfile = new File(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "prims.txt");
@@ -118,9 +123,38 @@ public class SecondPrims {
     }
 
     void printMST(int parent[], int graph[][]) {
+//        File result = new File(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "primsans.txt");
+//        result.delete();
+        try {
+            try (FileWriter myWriter = new FileWriter(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "primsans.txt", true)) {
+                myWriter.write("Edge \tWeight\n");
+                for (int i = 1; i < node; i++) {
+                    myWriter.write(parent[i] + " − " + i + "\t" + graph[i][parent[i]] + "\n");
+                }
+                myWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+
         System.out.println("Edge \tWeight");
         for (int i = 1; i < node; i++) {
             System.out.println(parent[i] + " − " + i + "\t" + graph[i][parent[i]]);
+        }
+
+    }
+
+    void dateWrite() {
+        try {
+            try (FileWriter myWriter = new FileWriter(System.getProperty("user.home") + File.separator + "Desktop" + File.separator + "primsans.txt", true)) {
+                LocalDateTime dt = LocalDateTime.now();
+                DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy  HH-mm");
+                String fdt = dt.format(f);
+                myWriter.write("\n"+fdt+"  --\n");
+                myWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
         }
 
     }
