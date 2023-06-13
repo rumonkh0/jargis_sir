0fed13fde6844de51c69ba35ef39c8be5c4a13a4
rumon <rummankh0@gmail.com>
2023-06-14 00:57:55 +0600

    sldfkj

--- src/algo_lab_jargis_sir/QuicksortStepByStep.java
+++ src/algo_lab_jargis_sir/QuicksortStepByStep.java
@@ -35,35 +35,64 @@ class qcksort {
             }
 
             if (too_big_idx < too_small_idx) {
+                System.out.print("For creating partition with pivot " + arr[pivot] + " swaping " + (too_big_idx - l) + "th and " + (too_small_idx - l) + "th element of the array is ");
+
                 swap(arr, too_big_idx, too_small_idx);
+                for (int m = l; m <= r; m++) {
+                    System.out.print(arr[m] + " ");
+                }
+                System.out.println("");
             }
         }
 
         swap(arr, too_small_idx, pivot);
+        System.out.println("The pivot position is " + (too_small_idx - l) + " and swaping with " + (too_small_idx - l) + "th element");
+
         return too_small_idx;
     }
 
     void quickSort(int a[], int beg, int end) {
         int loc;
         if (beg < end) {
+            System.out.print("the partition array is: ");
+            for (int m = beg; m <= end; m++) {
+                System.out.print(a[m] + " ");
+            }
+            System.out.println("\nThe pivot is " + a[beg]);
             loc = partition(a, beg, end);
+//            System.out.println("The pivot position is " + (loc + 1) + " and swaping with " + (loc + 1) + "th element");
+            System.out.print("now the two partition is: ");
+            for (int m = beg; m < loc; m++) {
+                System.out.print(a[m] + " ");
+            }
+            System.out.print("|" + a[loc] + "| ");
+            for (int m = loc + 1; m <= end; m++) {
+                System.out.print(a[m] + " ");
+            }
+            System.out.println("\n");
             quickSort(a, beg, loc - 1);
             quickSort(a, loc + 1, end);
         }
     }
 }
 
-public class Quicksort {
-
-    static int n = 10;
+public class QuicksortStepByStep {
+    static int n=10;
 
     public static void main(String[] args) {
+//        int[] arr = {40, 20, 10, 80, 60, 50, 70, 30, 90, 100};
+//        int[] arr = {5,  9, 11, 15,18, 20, 26};
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+//        int size = sc.nextInt();
+//        int arr[]= new int[size];
+//        for(int i=0; i<size; i++){
+//            arr[i]=sc.nextInt();
+//        }
         int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = (int) (Math.random() * 100);
+        for(int i=0; i<n; i++){
+            arr[i]=(int) (Math.random()*1000);
         }
         qcksort qs = new qcksort();
         qs.quickSort(arr, 0, arr.length - 1);
